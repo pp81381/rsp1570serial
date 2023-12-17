9df2d25cc65b076ea49a00589db236b72498cde7
Phil Porter <github@philporter.co.uk>
2023-12-17 18:52:52 +0000

    Updated setup.py to reflect new deps

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-import setuptools
+import setuptools  # type: ignore[import-untyped]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="rsp1570serial-pp81381",
-    use_scm_version={"local_scheme": "dirty-tag",},  # For PyPi compliance
+    use_scm_version={
+        "local_scheme": "dirty-tag",
+    },  # For PyPi compliance
     setup_requires=["setuptools_scm"],
     author="Phil Porter",
     description="Rotel RSP-1570 processor asyncio RS-232 protocol",
@@ -16,6 +18,9 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Terminals :: Serial",
@@ -23,6 +28,10 @@ setuptools.setup(
         "Framework :: AsyncIO",
         "Development Status :: 4 - Beta",
     ],
-    python_requires="~=3.7",
-    install_requires=["pyserial>=3.4", "pyserial_asyncio>=0.4", "aiounittest>=1.1.0"],
+    python_requires=">=3.7",
+    install_requires=[
+        "pyserial>=3.4",
+        "pyserial-asyncio-fast>=0.11",
+        "aiounittest>=1.1.0",
+    ],
 )
